b8f94a09bb87764bba87f12cc6cf83e6d310b55e
ViniciusFalcioni <viniciuskrilow@gmail.com>
2024-09-03 21:15:44 -0300

    Projeto Web - Jeferson

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => (
-    <h1>Home Works</h1>
-)
+    <div>
+        <h1>O que é uma To-Do List?</h1>
+        <p>
+            Uma <strong>to-do list</strong> (ou lista de tarefas) é uma ferramenta simples e eficaz usada para organizar e gerenciar tarefas e compromissos. Ela pode ser física, como um caderno ou uma folha de papel, ou digital, através de aplicativos e plataformas online.
+        </p>
+        <h2>Como Funciona?</h2>
+        <p>
+            A to-do list funciona listando todas as tarefas que você precisa realizar. A ideia é registrar as atividades para não esquecer e poder acompanhar o progresso. Aqui estão algumas características comuns de uma to-do list:
+        </p>
+        <ul>
+            <li><strong>Listagem de Tarefas:</strong> Você escreve ou adiciona as tarefas que precisam ser realizadas.</li>
+            <li><strong>Priorização:</strong> Algumas listas permitem que você classifique as tarefas por ordem de prioridade.</li>
+            <li><strong>Marcar como Concluído:</strong> À medida que você completa uma tarefa, pode marcá-la como concluída, o que ajuda a visualizar seu progresso.</li>
+            <li><strong>Atualizações:</strong> Você pode adicionar novas tarefas ou atualizar o status das existentes conforme necessário.</li>
+        </ul>
+        <p>
+            Usar uma to-do list pode ajudar a aumentar a produtividade e a reduzir o estresse, mantendo você organizado e focado em suas tarefas diárias.
+        </p>
+    </div>
+);
 
 export default Home;
--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,7 +1,62 @@
 import React from "react";
 
 const Sobre = () => (
-    <h1>Sobre Works</h1>
-)
+    <div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <h1>Olá, <span>eu sou Vinicius.</span></h1>
+                        <p>Seja bem-vindo ao meu portfólio!</p>
+                        <p>Sou desenvolvedor web tenho 2 anos de experiência na área.<br />Estou entusiasmado em compartilhar um pouco sobre minha jornada e habilidades com vocês.</p>
+                        <a href='#about'>Sobre mim</a>
+                    </div>
+                </div>
+                <div>
+                    
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+
+            <div id='about'>
+                <div>
+                    <div>
+                        <div>
+
+                            <div>
+                                <div>
+                                </div>
+                                <p>Vinicius Falcioni</p>
+                                <p>Desenvolvedor Front-end</p>
+                                <p>Cascavel/PR</p>
+                                <div>
+                                    <a target='_blank' href="https://www.linkedin.com/in/vinicius-falcioni">Linkedin</a>
+                                    <a target='_blank' href="https://github.com/ViniciusFalcioni">Github</a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div>
+                                <h2>Sobre mim</h2>
+                            </div>
+
+                            <p>Olá! Meu nome é Vinicius Krilow Falcioni, e sou um desenvolvedor web apaixonado pela área da tecnologia.Tenho 20 anos de idade, estou sempre em busca de novos desafios e oportunidades para aplicar meus conhecimentos</p>
+                            <p>Atualmente, estou em busca de novas oportunidades profissionais. Durante minha jornada no mundo do desenvolvimento web, fiquei surpreso pela maneira como a tecnologia está em constante evolução, trazendo benefícios para diversas áreas do mercado de trabalho.</p>
+                            <p>Minha jornada profissional começou na cidade de Salto do Lontra, no Paraná, onde cresci e desenvolvi interesse pelo área da tecnologia. Atualmente, estou residindo em Cascavel, também no Paraná, Pois estou cursando a faculdade de Engenharia de Software, na qual estou no 2º período.</p>
+                            <p>Com 2 anos de experiência no ramo de Desenvolvimento Web, tive a oportunidade de trabalhar em duas empresas notáveis, onde aprimorei minhas habilidades e adquiri valiosa experiência. Durante 2 anos, atuei como Suporte Técnico na empresa Imonov, que é especializada em Soluções Imobiliárias. Nessa função, fui responsável por oferecer suporte aos clientes, solucionar problemas técnicos e garantir a satisfação dos usuários. Essa experiência me proporcionou um entendimento profundo das necessidades do cliente e a importância de fornecer soluções eficientes.</p>
+                            <p>Além disso, tive a oportunidade de trabalhar como Desenvolvedor Web na Fluency Academy, onde atuei por 6 meses. Nessa empresa inovadora, pude aprimorar minhas habilidades técnicas, colaborando no desenvolvimento de soluções web e contribuindo para melhorar a experiência dos usuários em uma plataforma de ensino online.</p>
+                            <p>Estou comprometido em continuar aprimorando minhas habilidades e conhecimentos, tanto na faculdade como em projetos profissionais e pessoais. Acredito que a aprendizagem contínua é essencial para se manter atualizado no mundo da tecnologia em constante evolução.</p>
+                            <p>Olhando para o futuro, Penso em crescer como profissional e de construir minha própria empresa no ramo da tecnologia.</p>
+                        </div>
+
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
 export default Sobre;
